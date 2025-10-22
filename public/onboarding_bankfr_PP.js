3df8ecb4e477ba8aaaeeae809889eeee6d9323f3
hadilromdhane-cloud <hadil.romdhane@vneuron.com>
2025-10-22 15:25:11 +0100

    Traduction3

--- public/onboarding_bankfr_PP.js
+++ public/onboarding_bankfr_PP.js
@@ -698,7 +698,7 @@ fetchRiskLevel: async function(riskCalculationId) {
         { screeningField: 'firstName', onboardingFields: ['prenom', 'firstName'], readonly: true, label: 'Prénom/First Name' },
         { screeningField: 'lastName', onboardingFields: ['nom', 'lastName'], readonly: true, label: 'Nom/Last Name' },
         { screeningField: 'birthDate', onboardingFields: ['dateNaissance', 'dateOfBirth'], readonly: true, label: 'Date de naissance/Birth Date' },
-        { screeningField: 'nationality', onboardingFields: ['nationalite', 'nationality',], readonly: true, label: 'Nationalité/Nationality', fieldType: 'select' },
+        { screeningField: 'nationality', onboardingFields: ['Nationalite', 'nationality',], readonly: true, label: 'Nationalité/Nationality', fieldType: 'select' },
         { screeningField: 'citizenship', onboardingFields: ['PaysDeResidence', 'countryOfResidence','citizenship'], readonly: true, label: 'Pays de Résidence/Country of Residence', fieldType: 'select' }
     ];
         let fieldsLocked = 0;
